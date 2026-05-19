320618b4b80f872c99edd7bb795f15940e960d3f
keofc <keoitc2014@gmail.com>
2026-05-19 09:20:14 +0700

    ddd

--- app/(print)/service-admin/work-orders/print/[id]/page.tsx
+++ app/(print)/service-admin/work-orders/print/[id]/page.tsx
@@ -3,7 +3,7 @@ import PrintWorkOrder from "@/_screens/service/PrintWorkOrder";
 
 export default function Page() {
   return (
-    <AuthGuard roles={["service_admin", "service_manager", "account_admin", "head_technician"]}>
+    <AuthGuard>
       <PrintWorkOrder />
     </AuthGuard>
   );
